d5d2d7649445c5f93978762f9ac9bda7a90a8382
Vincent Thomas <77443389+vincent-thomas@users.noreply.github.com>
2024-09-11 00:42:17 +0200

    nice neovim config

---
+++ nvim/plugin/conform.lua
@@ -0,0 +1,22 @@
+
+local conform = require("conform")
+
+local M = {}
+
+local config = {
+  formatters_by_ft = {
+    lua = { "stylua" },
+    rust = { "rustfmt" },
+    javascript = { "prettierd" },
+    nix = { "nixfmt" },
+  },
+}
+
+conform.setup(config)
+
+vim.api.nvim_create_autocmd("BufWritePre", {
+  pattern = "*",
+  callback = function(args)
+    require("conform").format({ bufnr = args.buf })
+  end,
+})
