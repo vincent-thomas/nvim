77ad2f0180513a3028b0513e3ac79b97c86b92f0
Vincent Thomas <77443389+vincent-thomas@users.noreply.github.com>
2024-09-24 17:07:56 +0200

    disable c-p oil

--- nvim/plugin/oil.lua
+++ nvim/plugin/oil.lua
@@ -1,25 +1,23 @@
-
-require("oil").setup({
+require('oil').setup {
   default_file_explorer = true,
   columns = {
-    "icon",
+    'icon',
   },
   keymaps = {
-    ["g?"] = "actions.show_help",
-    ["<CR>"] = "actions.select",
-    ["<C-p>"] = "actions.preview",
-    ["<C-c>"] = "actions.close",
-    ["<C-l>"] = "actions.refresh",
-    ["-"] = "actions.parent",
-    ["_"] = "actions.open_cwd",
-    ["gs"] = "actions.change_sort",
-    ["t"] = "actions.open_external",
-    ["."] = "actions.toggle_hidden",
-    ["g\\"] = "actions.toggle_trash",
+    ['g?'] = 'actions.show_help',
+    ['<CR>'] = 'actions.select',
+    ['<C-c>'] = 'actions.close',
+    ['<C-l>'] = 'actions.refresh',
+    ['-'] = 'actions.parent',
+    ['_'] = 'actions.open_cwd',
+    ['gs'] = 'actions.change_sort',
+    ['t'] = 'actions.open_external',
+    ['.'] = 'actions.toggle_hidden',
+    ['g\\'] = 'actions.toggle_trash',
   },
-  preview_split = "left",
+  preview_split = 'left',
   filters = {
     dotfiles = false,
   },
-})
-vim.keymap.set("n", "-", "<cmd>Oil<cr>")
+}
+vim.keymap.set('n', '-', '<cmd>Oil<cr>')
