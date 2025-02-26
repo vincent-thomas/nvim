5f2103a8ddd2cc4202b043e9e0bbc61eb02a19d6
Vincent Thomas <77443389+vincent-thomas@users.noreply.github.com>
2025-02-26 16:53:21 +0100

    added cachix

--- flake.nix
+++ flake.nix
@@ -1,6 +1,15 @@
 {
   description = "Neovim derivation";
-
+  nixConfig = {
+    substituters = [
+      "https://nix-community.cachix.org"
+      "https://cache.nixos.org"
+    ];
+    trusted-public-keys = [
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+    ];
+  };
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
